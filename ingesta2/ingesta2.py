659d8b569b22c9662b50d574e47b6492b8175cd6
zamir5895 <lmr293141@gmail.com>
2024-10-04 12:59:54 -0500

    ingesta final

--- ingesta2/ingesta2.py
+++ ingesta2/ingesta2.py
@@ -1,9 +1,10 @@
-import mysql.connector
+import psycopg2
 import csv
 import boto3
 
 fichero_csv = None
 
+# Parámetros de conexión a PostgreSQL
 host = '98.83.127.213'
 port = 8006  
 database = 'gestion'
@@ -12,7 +13,8 @@ password = 'utec'
 tabla = 'producto'
 
 try:
-    conexion = mysql.connector.connect(
+    # Conectar a la base de datos PostgreSQL
+    conexion = psycopg2.connect(
         host=host,
         port=port,  
         database=database,
@@ -20,28 +22,33 @@ try:
         password=password
     )
 
-    if conexion.is_connected():
-        print(f'Conectado a la base de datos {database}')
-        cursor = conexion.cursor()
-        cursor.execute(f'SELECT * FROM {tabla}')
-        registros = cursor.fetchall()
-        columnas = [i[0] for i in cursor.description]
-        fichero_csv = 'producto.csv'
-        
-        with open(fichero_csv, 'w', newline='') as archivo_csv:
-            escritor_csv = csv.writer(archivo_csv)
-            escritor_csv.writerow(columnas)
-            escritor_csv.writerows(registros)
-        print(f'Registros guardados en {fichero_csv}')
+    print(f'Conectado a la base de datos {database}')
+    cursor = conexion.cursor()
 
-except mysql.connector.Error as error:
-    print(f'Error al conectar a MySQL: {error}')
+    # Ejecutar consulta para obtener los datos
+    cursor.execute(f'SELECT * FROM {tabla}')
+    registros = cursor.fetchall()
+    columnas = [desc[0] for desc in cursor.description]
+
+    # Generar archivo CSV
+    fichero_csv = 'producto.csv'
+    with open(fichero_csv, 'w', newline='') as archivo_csv:
+        escritor_csv = csv.writer(archivo_csv)
+        escritor_csv.writerow(columnas)
+        escritor_csv.writerows(registros)
+
+    print(f'Registros guardados en {fichero_csv}')
+
+except psycopg2.Error as error:
+    print(f'Error al conectar a PostgreSQL: {error}')
 finally:
-    if 'conexion' in locals() and conexion.is_connected():
+    # Cerrar la conexión a la base de datos si está abierta
+    if 'conexion' in locals() and conexion:
         cursor.close()
         conexion.close()
-        print('Conexión MySQL cerrada')
+        print('Conexión PostgreSQL cerrada')
 
+# Subir el archivo CSV a S3
 if fichero_csv:
     nombreBucket = "contenedor2zamir"
     s3 = boto3.client('s3')
